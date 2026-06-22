cb38bb9c0b272b3d0675f89c170dee1a223b8b50
L-S16 <sorialuwis@gmail.com>
2026-06-22 08:16:44 -0500

    OP-27 modelo pedido

---
+++ lib/models/pedido.dart
@@ -0,0 +1,15 @@
+class Pedido {
+  String id;
+  String cliente;
+  String direccion;
+  String prioridad;
+  String estado;
+
+  Pedido({
+    required this.id,
+    required this.cliente,
+    required this.direccion,
+    required this.prioridad,
+    required this.estado,
+  });
+}
